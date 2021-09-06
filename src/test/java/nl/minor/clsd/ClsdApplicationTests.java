7eab8046b7c884b56a872475a491fc3fc50e3925
Redouanelh <redouan_school@outlook.com>
2021-09-06 14:18:57 +0200

    initialize spring

---
+++ src/test/java/nl/minor/clsd/ClsdApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.minor.clsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClsdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
