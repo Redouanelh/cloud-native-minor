530b23d64eb2feb53ec77aa6837eff31ed230fe9
Redouanelh <redouan_school@outlook.com>
2021-09-07 16:19:51 +0200

    init assignment 3

---
+++ src/main/java/nl/minor/clsd/ClsdApplication.java
@@ -0,0 +1,13 @@
+package nl.minor.clsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClsdApplication.class, args);
+	}
+
+}
