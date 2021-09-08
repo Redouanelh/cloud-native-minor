019c3597a6af59154ee482e6b1da065791244d96
Redouanelh <redouan_school@outlook.com>
2021-09-08 17:26:54 +0200

    start with repo

--- src/main/java/nl/minor/clsd/repository/AccountHolderRepository.java
+++ src/main/java/nl/minor/clsd/repository/AccountHolderRepository.java
@@ -1,7 +1,13 @@
 package nl.minor.clsd.repository;
 
+import nl.minor.clsd.domain.Account;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class AccountHolderRepository {
+    private Map<Integer, List<Account>> accountHoldersLocalStorage = new HashMap<>();
 }
--- src/main/java/nl/minor/clsd/repository/AccountRepository.java
+++ src/main/java/nl/minor/clsd/repository/AccountRepository.java
@@ -1,7 +1,13 @@
 package nl.minor.clsd.repository;
 
+import nl.minor.clsd.domain.AccountHolder;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class AccountRepository {
+    private Map<Integer, List<AccountHolder>> accountsLocalStorage = new HashMap<>();
 }
