6c7915b724aaa8a99dd4c35f10d6e4feba07930c
Redouanelh <redouan_school@outlook.com>
2021-09-20 15:05:17 +0200

    more stuff

--- src/main/java/nl/minor/clsd/application/DummyApiService.java
+++ src/main/java/nl/minor/clsd/application/DummyApiService.java
@@ -1,10 +1,18 @@
 package nl.minor.clsd.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class DummyApiService {
 
+//    @Autowired
 //    private final RestTemplate restTemplate;
+
+//    public DummyApiService(RestTemplate restTemplate) {
+////        this.restTemplate = restTemplate;
+////    }
+
+
 }
